5bb5ac5d9ae4257a816fa94fdc7b3b2db0b5665b
anirudhs-1010 <asunil3@illinois.edu>
2026-01-11 11:54:55 -0800

    Taking out the images to make it load faster

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ import CertificationCards from './comp/certificate';
 import ContactForms from './comp/contact';
 import Footer from './comp/footer';
 import Header from './comp/header';
-import Hero from './comp/hero';
 import Projects from './comp/projects';
 import Skills from './comp/skills';
 import Experience from './comp/work';
@@ -19,7 +18,6 @@ function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-white font-montserrat ">
       <Header/>
-      <Hero/>
       <About/>
       <Experience />
       <Projects />
